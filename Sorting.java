37ecb686d62b0c2ee4ff0ea55fbdd8a8f104ec75
Zach <zachderhake@gmail.com>
2024-09-10 14:16:57 -0700

    ;

--- Sorting.java
+++ Sorting.java
@@ -16,5 +16,48 @@ public class Sorting
                     unsorted[k] = swap;
                 }
             }
-        } }
+        } 
+    }
+    
+    public static void selectionSort(int[] arr)
+    {
+        //find minamum 
+        //put it at the front. (swap) 
+        //adjust for loop
+        
+        for (int i = 0; i < arr.length; i++)
+        {
+            //find minamum algo
+            int min = arr[i];
+            int minIndex = i; 
+            
+            for (int k = i+1; k < arr.length; k++)
+            {
+                if (arr[k] < min)
+                {
+                    min = arr[k];
+                    minIndex = k; 
+                }
+            }
+            int swap = arr[i];
+            arr[i] = min;
+            arr[minIndex] = swap;
+        }
+        
+    }
+    public static void insertionSort(int[] arr)
+    {
+        for (int i = 1; i < arr.length; i++)
+        {
+            int k = i - 1; 
+            int iValue = arr[i];
+            while (k >= 0 && iValue < arr[k])
+            {
+                int swap = arr[k+1];
+                arr[k+1] = arr[k];
+                arr[k] = swap;
+                k--;
+            }
+        }  
+    }
  }      
--- Test.java
+++ Test.java
@@ -1,10 +1,37 @@
+
 public class Test
 {
     public static void testBubble()
     {
         int[] arr = {95, 80, 99, 88, 47, 77, 5, 37,  37, 9, 92, 87, 58, 65, 91, 28, 73, 59, 72, 48, 76, 85, 23, 36, 75, 93, 43, 8, 22, 96, 78, 33, 70, 16, 21, 2,
              41, 6, 20, 7, 10, 98, 1, 42, 83, 71, 54, 67, 17, 50, 100, 49, 19};
-        Methods.bubbleSort(arr);
+        Sorting.bubbleSort(arr);
+        String aStr = " ";
+        for (int i = 0; i < arr.length; i++)
+        {
+            aStr = aStr + arr[i] + " ";
+        }
+        System.out.println(aStr);
+        
+    }
+    
+    public static void testSelection()
+    {
+        int[] arr = {95, 80, 99, 88, 47, 77, 5, 37,  37};
+        Sorting.selectionSort(arr);
+        String aStr = " ";
+        for (int i = 0; i < arr.length; i++)
+        {
+            aStr = aStr + arr[i] + " ";
+        }
+        System.out.println(aStr);
+        
+    }
+    
+    public static void testInsertion()
+    {
+        int[] arr = {95, 80, 99, 88, 47, 77, 5, 37,  37};
+        Sorting.insertionSort(arr);
         String aStr = " ";
         for (int i = 0; i < arr.length; i++)
         {
