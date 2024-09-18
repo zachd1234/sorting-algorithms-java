3cb55f35d8550c7bb29f9bea28598ed5f38782cf
Zach <zachderhake@gmail.com>
2024-09-18 10:07:18 -0700

    tracking my performance. It works!

--- Sorting.java
+++ Sorting.java
@@ -1,72 +1,99 @@
 public class Sorting
 {
+    private static long comparison = 0;
+    private static long assignment = 0;
     // instance variables - replace the example below with your own
-    //fg
+    //fg  -  public static void bubbleSort(int[] arr)-  public static void selectionSort(int[] arr)
 
-    public static void bubbleSort(int[] unsorted)
+  
+
+    public static void bubbleSort(int[] arr)
     {
-        for(int i = 0; i <unsorted.length; i++)
+        comparison = 0;
+        assignment = 0;
+        
+        for(int i = 0; i <arr.length; i++)
         {
-            for (int k = 0; k < unsorted.length - 1 - i; k++)
+            for (int k = 0; k < arr.length - 1 - i; k++)
             {
-                if (unsorted[k] > unsorted[k+1])
+                comparison++;
+                if (arr[k] > arr[k+1])
                 {
-                    int swap = unsorted[k+1];
-                    unsorted[k+1] = unsorted[k];
-                    unsorted[k] = swap;
+                    int swap = arr[k+1];
+                    arr[k+1] = arr[k];
+                    arr[k] = swap;
+                    assignment += 3; 
                 }
             }
         } 
+        System.out.print(", " + comparison + ", " + assignment);
     }
-    
     public static void selectionSort(int[] arr)
     {
         //find minamum 
         //put it at the front. (swap) 
         //adjust for loop
+        comparison = 0;
+        assignment = 0;
         
         for (int i = 0; i < arr.length; i++)
         {
             //find minamum algo
             int min = arr[i];
             int minIndex = i; 
+            assignment += 2;
             
             for (int k = i+1; k < arr.length; k++)
             {
+                comparison++;
                 if (arr[k] < min)
                 {
                     min = arr[k];
-                    minIndex = k; 
+                    minIndex = k;
+                    assignment += 2;
                 }
             }
             int swap = arr[i];
             arr[i] = min;
             arr[minIndex] = swap;
+            assignment += 3;
         }
-        
+        System.out.print(", " + comparison + ", " + assignment);
     }
     public static void insertionSort(int[] arr)
     {
+        comparison = 0;
+        assignment = 0;
         for (int i = 1; i < arr.length; i++)
         {
             int k = i - 1; 
             int iValue = arr[i];
+            assignment += 2;
+            //could do assignment + 1 
+            comparison++;
             while (k >= 0 && iValue < arr[k])
             {
                 int swap = arr[k+1];
                 arr[k+1] = arr[k];
                 arr[k] = swap;
                 k--;
+                assignment += 3;
+                comparison++;
             }
-        }  
+        }
+        System.out.print(", " + comparison + ", " + assignment);
     }
     
     public static void mergeSort(int[] arr)
     {
+        comparison = 0;
+        assignment = 0;
         mergeSort(arr, 0, arr.length-1);
+        System.out.print(", " + comparison + ", " + assignment);        
     }
     public static void mergeSort(int[] arr, int beg, int end)
     {
+        comparison++;
         if (beg == end)
         {
             return;
@@ -77,6 +104,7 @@ public class Sorting
             mergeSort(arr, beg, mid);
             mergeSort(arr, mid + 1, end);
             merge(arr, beg, mid, end);
+            assignment += 4;
         }
     }
     public static void merge(int[] arr, int beg, int mid, int end)
@@ -84,47 +112,63 @@ public class Sorting
         int[] temp = new int[end - beg + 1 ]; //arr.length - 1? 
         int tempCur = 0;
         int rightCur = mid + 1;
-        int leftCur = beg; 
+        int leftCur = beg;
+     
+        comparison++;
         while (leftCur <= mid && rightCur <= end)
         {
+            comparison++;
             if (arr[leftCur] < arr[rightCur])
             {
                 temp[tempCur] = arr[leftCur];
                 tempCur++;
                 leftCur++;
+                assignment ++;
             }
             else
             {
                 temp[tempCur] = arr[rightCur];
                 tempCur++;
                 rightCur++;
+                assignment++;
             }
+            comparison++;
         }
-        
+        comparison++;
         while (leftCur <= mid)
         {
             temp[tempCur] = arr[leftCur];
             tempCur++;
             leftCur++;
+            assignment ++;
+            comparison++;
         }
+        comparison++;
         while (rightCur <= end)
         {
             temp[tempCur] = arr[rightCur];
             tempCur++;
             rightCur++;
+            assignment ++;
+            comparison++;
         }   
         //copy temp over to array
         for (int k = 0; k < temp.length; k++)
         {
             arr[k+beg] = temp[k];
+            assignment++;
         }
     }
     public static void quickSort(int[] arr)
     {
+        comparison = 0;
+        assignment = 0;
         quickSort(arr, 0, arr.length-1);
+        System.out.print(", " + comparison + ", " + assignment);
     }
     public static void quickSort(int[] arr, int beg, int end)
     {
+        comparison++;
         if (end <= beg)
         {
             return;
@@ -135,6 +179,7 @@ public class Sorting
             int pivotIndex = partition(arr, beg, end);
             quickSort(arr, beg, pivotIndex -1);
             quickSort(arr, pivotIndex+1, end);
+            assignment += 3;
         }
     }
     public static int partition(int[] arr, int beg, int end)
@@ -146,25 +191,36 @@ public class Sorting
         arr[pivotIndex] = swap;
         
         int pivotVal = arr[end];
+        assignment += 4; 
+        
         int leftCur = beg;
         int rightCur = end - 1;
         
         do
         {
+            comparison++; 
             while(arr[leftCur] < pivotVal)
             {
                 leftCur++;
+                comparison++;
             }
+            comparison++;
             while (arr[rightCur] > pivotVal && rightCur > beg)
             {
                 rightCur--;
+                comparison++;
             }
+            comparison++;
             if (leftCur < rightCur)
             {
                 int swap3 = arr[leftCur];
                 arr[leftCur] = arr[rightCur];
-                arr[rightCur] = swap3; 
+                arr[rightCur] = swap3;
+                leftCur++;
+                rightCur--;
+                assignment += 3;
             }
+            comparison++;
         }
         while(leftCur < rightCur);
         
@@ -174,7 +230,7 @@ public class Sorting
          int swap2 = arr[end];
          arr[end] = arr[pivotIndex];
          arr[pivotIndex] = swap2; 
-         
+         assignment += 3; 
          return pivotIndex; 
     }
  }      
---
+++ SortingBenchmarkV1.java
@@ -0,0 +1,233 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Class to analyze the performance of sort algorithms.
+ *
+ * @author Mr. Murphy
+ * @version 2024/09/13
+ */
+public class SortingBenchmarkV1
+{
+    // Run all five sort methods
+    // Using four different sizes of arrays
+    // Using three different initial conditions
+    public static void runBenchmark() {
+        // verify sorts handle base conditions
+        int[] testArray = {1};
+        Sorting.bubbleSort(testArray);
+        Sorting.selectionSort(testArray);
+        Sorting.insertionSort(testArray);
+        Sorting.mergeSort(testArray);
+        Sorting.quickSort(testArray);
+        System.out.println("\nFirst test passed\n");
+        
+        testArray = new int[] {1, 1, 1};
+        Sorting.bubbleSort(testArray);
+        Sorting.selectionSort(testArray);
+        Sorting.insertionSort(testArray);
+        Sorting.mergeSort(testArray);
+        Sorting.quickSort(testArray);
+        System.out.println("\nSecond test passed\n");
+        
+        // verify sorts are accurate
+        testArray = new int[100];
+        int[] sortedTestArray = new int[100];
+        populateRandomArray(testArray);
+        populateRandomArray(sortedTestArray);
+        Arrays.sort(sortedTestArray);
+        
+        Sorting.bubbleSort(testArray);
+        System.out.println("\nBubble Sort is accurate: " 
+                + Arrays.equals(testArray, sortedTestArray));
+        populateRandomArray(testArray);
+        Sorting.selectionSort(testArray);
+        System.out.println("\nSelection Sort is accurate: " 
+                + Arrays.equals(testArray, sortedTestArray));
+        populateRandomArray(testArray);
+        Sorting.insertionSort(testArray);
+        System.out.println("\nInsertion Sort is accurate: " 
+                + Arrays.equals(testArray, sortedTestArray));
+        populateRandomArray(testArray);
+        Sorting.mergeSort(testArray);
+        System.out.println("\nMerge Sort is accurate: "
+                + Arrays.equals(testArray, sortedTestArray));
+        populateRandomArray(testArray);
+        Sorting.quickSort(testArray);
+        System.out.println("\nQuick Sort is accurate: "
+                + Arrays.equals(testArray, sortedTestArray));
+        
+        // run benchmarks
+        System.out.println("\nSize, Initial, Sort, Compares, Assigns, Time");
+        int[] sizes = {25000, 50000, 75000, 100000};
+        for (int size : sizes) {
+            int[] arrayToSort = new int[size];
+            long curTime;
+
+            // randomly-sorted array
+            populateRandomArray(arrayToSort);
+            System.out.print(size + ", random, bubble");
+            curTime = System.currentTimeMillis();
+            Sorting.bubbleSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            populateRandomArray(arrayToSort);
+            System.out.print(size + ", random, selection");
+            curTime = System.currentTimeMillis();
+            Sorting.selectionSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+            
+            populateRandomArray(arrayToSort);
+            System.out.print(size + ", random, insertion");
+            curTime = System.currentTimeMillis();
+            Sorting.insertionSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            populateRandomArray(arrayToSort);
+            System.out.print(size + ", random, merge");
+            curTime = System.currentTimeMillis();
+            Sorting.mergeSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            populateRandomArray(arrayToSort);
+            System.out.print(size + ", random, quick");
+            curTime = System.currentTimeMillis();
+            Sorting.quickSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            // almost-sorted array -- 0.1% not sorted
+            populateAlmostSortedArray(arrayToSort);
+            System.out.print(size + ", almost, bubble");
+            curTime = System.currentTimeMillis();
+            Sorting.bubbleSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            populateAlmostSortedArray(arrayToSort);
+            System.out.print(size + ", almost, selection");
+            curTime = System.currentTimeMillis();
+            Sorting.selectionSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            populateAlmostSortedArray(arrayToSort);
+            System.out.print(size + ", almost, insertion");
+            curTime = System.currentTimeMillis();
+            Sorting.insertionSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            populateAlmostSortedArray(arrayToSort);
+            System.out.print(size + ", almost, merge");
+            curTime = System.currentTimeMillis();
+            Sorting.mergeSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            populateAlmostSortedArray(arrayToSort);
+            System.out.print(size + ", almost, quick");
+            curTime = System.currentTimeMillis();
+            Sorting.quickSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            // reverse-sorted array -- worst case
+            populateReversedArray(arrayToSort);
+            System.out.print(size + ", reverse, bubble");
+            curTime = System.currentTimeMillis();
+            Sorting.bubbleSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+            
+            populateReversedArray(arrayToSort);
+            System.out.print(size + ", reverse, selection");
+            curTime = System.currentTimeMillis();
+            Sorting.selectionSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+            
+            populateReversedArray(arrayToSort);
+            System.out.print(size + ", reverse, insertion");
+            curTime = System.currentTimeMillis();
+            Sorting.insertionSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+            
+            populateReversedArray(arrayToSort);
+            System.out.print(size + ", reverse, merge");
+            curTime = System.currentTimeMillis();
+            Sorting.mergeSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            populateReversedArray(arrayToSort);
+            System.out.print(size + ", reverse, quick");
+            curTime = System.currentTimeMillis();
+            Sorting.quickSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+        }
+
+        int[] sizes2 = {10000000, 20000000, 30000000, 40000000,
+                        50000000, 60000000, 70000000, 80000000};
+        for (int size : sizes2) {
+            int[] arrayToSort = new int[size];
+            long curTime;
+            
+            populateRandomArray(arrayToSort);
+            System.out.print(size + ", random, merge");
+            curTime = System.currentTimeMillis();
+            Sorting.mergeSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+
+            populateRandomArray(arrayToSort);
+            System.out.print(size + ", random, quick");
+            curTime = System.currentTimeMillis();
+            Sorting.quickSort(arrayToSort);
+            System.out.println(", " + (System.currentTimeMillis() - curTime) / 1000.0);
+        }
+
+    }
+    
+    private static void populateRandomArray(int[] arr) {
+        Random rng = new Random(5000); // seed it to produced repeatable results
+        int boundary = arr.length * 10; // spread values to reduce duplicates
+        
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = rng.nextInt(boundary);
+        }
+    }
+    
+    private static void populateAlmostSortedArray(int[] arr) {
+        Random rng = new Random(5000); // seed it to produced repeatable results
+        int boundary = arr.length * 10; // spread values to reduce duplicates
+        
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = rng.nextInt(boundary);
+        }
+        
+        Arrays.sort(arr);
+        
+        // now change 0.1% of numbers
+        for (int i = arr.length/1000; i < arr.length; i += arr.length/1000) {
+            arr[i] = rng.nextInt(boundary);
+        }
+    }
+    
+    private static void populateReversedArray(int[] arr) {
+        Random rng = new Random(5000); // seed it to produced repeatable results
+        int boundary = arr.length * 10; // spread values to reduce duplicates
+        
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = rng.nextInt(boundary);
+        }
+        
+        Arrays.sort(arr);
+        
+        // now swap values to other side of array
+        for (int i=0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length-i-1];
+            arr[arr.length-i-1] = temp;
+        }
+    }
+    
+    private static boolean isSorted(int[] arr) {
+        for (int i=0; i<arr.length-1; i++) {
+            if (arr[i] > arr[i+1]) {// not in order
+                return false; // only need one exception
+            }
+        }
+        return true; // no comparison was out of order
+    }
+}
