053290232a1e1f9d12aef1c922b19791054d75fd
Zach Derhake <zachderhake@gmail.com>
2024-10-01 17:35:47 -0700

    coding conventions!

--- Sorting.java
+++ Sorting.java
@@ -5,233 +5,218 @@ public class Sorting
     // instance variables - replace the example below with your own
     //fg  -  public static void bubbleSort(int[] arr)-  public static void selectionSort(int[] arr)
 
-  
-
-    public static void bubbleSort(int[] arr)
-    {
+    public static void bubbleSort(int[] arr) {
         comparison = 0;
         assignment = 0;
         
-        for(int i = 0; i <arr.length; i++)
-        {
-            for (int k = 0; k < arr.length - 1 - i; k++)
-            {
+        for(int i = 0; i <arr.length; i++) {
+            for (int k = 0; k < arr.length - 1 - i; k++) {
                 comparison++;
-                if (arr[k] > arr[k+1])
-                {
+                if (arr[k] > arr[k+1]) {
                     int swap = arr[k+1];
                     arr[k+1] = arr[k];
                     arr[k] = swap;
+                    
                     assignment += 3; 
                 }
             }
         } 
         System.out.print(", " + comparison + ", " + assignment);
     }
-    public static void selectionSort(int[] arr)
-    {
+    
+    public static void selectionSort(int[] arr) {
         //find minamum 
         //put it at the front. (swap) 
         //adjust for loop
         comparison = 0;
         assignment = 0;
         
-        for (int i = 0; i < arr.length; i++)
-        {
+        for (int i = 0; i < arr.length; i++) {
             //find minamum algo
             int min = arr[i];
             int minIndex = i; 
-            assignment += 2;
             
-            for (int k = i+1; k < arr.length; k++)
-            {
+            assignment += 2;
+            for (int k = i+1; k < arr.length; k++) {
                 comparison++;
-                if (arr[k] < min)
-                {
+                if (arr[k] < min) {
                     min = arr[k];
                     minIndex = k;
+                    
                     assignment += 2;
                 }
             }
             int swap = arr[i];
             arr[i] = min;
             arr[minIndex] = swap;
+            
             assignment += 3;
         }
         System.out.print(", " + comparison + ", " + assignment);
     }
-    public static void insertionSort(int[] arr)
-    {
+    
+    public static void insertionSort(int[] arr) {
         comparison = 0;
         assignment = 0;
-        for (int i = 1; i < arr.length; i++)
-        {
+        
+        for (int i = 1; i < arr.length; i++) {
             int k = i - 1; 
-            int iValue = arr[i];
+            int insrtValue = arr[i];
             //could do assignment + 1 
             comparison++;
-            while (k >= 0 && iValue < arr[k])
-            {
-                //
-                
+            while (k >= 0 && insrtValue < arr[k]) {
                 arr[k+1] = arr[k];
-                
                 k--;
+                
                 assignment++;
                 comparison++;
             }
-            arr[k+1] = iValue;
+            arr[k+1] = insrtValue;
+            
             assignment++;
         }
         System.out.print(", " + comparison + ", " + assignment);
     }
     
-    public static void mergeSort(int[] arr)
-    {
+    public static void mergeSort(int[] arr) {
         comparison = 0;
         assignment = 0;
+        
         mergeSort(arr, 0, arr.length-1);
         System.out.print(", " + comparison + ", " + assignment);        
     }
-    public static void mergeSort(int[] arr, int beg, int end)
-    {
+    
+    public static void mergeSort(int[] arr, int beg, int end) {
         comparison++;
-        if (beg == end)
-        {
+        if (beg == end) {
             return;
-        }
-        else
-        {
+        } else {
             int mid = (beg + end)/2;
             mergeSort(arr, beg, mid);
             mergeSort(arr, mid + 1, end);
             merge(arr, beg, mid, end);
+            
             assignment += 4;
         }
     }
+    
     public static void merge(int[] arr, int beg, int mid, int end)
     {
         int[] temp = new int[end - beg + 1 ]; //arr.length - 1? 
         int tempCur = 0;
         int rightCur = mid + 1;
         int leftCur = beg;
-     
+        
         comparison++;
-        while (leftCur <= mid && rightCur <= end)
-        {
+        while (leftCur <= mid && rightCur <= end) {
             comparison++;
-            if (arr[leftCur] < arr[rightCur])
-            {
+            if (arr[leftCur] < arr[rightCur]) {
                 temp[tempCur] = arr[leftCur];
                 tempCur++;
                 leftCur++;
+                
                 assignment ++;
-            }
-            else
-            {
+            } else {
                 temp[tempCur] = arr[rightCur];
                 tempCur++;
                 rightCur++;
+                
                 assignment++;
             }
             comparison++;
         }
         comparison++;
-        while (leftCur <= mid)
-        {
+        while (leftCur <= mid) {
             temp[tempCur] = arr[leftCur];
             tempCur++;
             leftCur++;
+            
             assignment ++;
             comparison++;
         }
         comparison++;
-        while (rightCur <= end)
-        {
+        while (rightCur <= end) {
             temp[tempCur] = arr[rightCur];
             tempCur++;
             rightCur++;
+            
             assignment ++;
             comparison++;
         }   
         //copy temp over to array
-        for (int k = 0; k < temp.length; k++)
-        {
+        for (int k = 0; k < temp.length; k++) {
             arr[k+beg] = temp[k];
+            
             assignment++;
         }
     }
-    public static void quickSort(int[] arr)
-    {
+    
+    public static void quickSort(int[] arr) {
         comparison = 0;
         assignment = 0;
+        
         quickSort(arr, 0, arr.length-1);
         System.out.print(", " + comparison + ", " + assignment);
     }
-    public static void quickSort(int[] arr, int beg, int end)
-    {
+    
+    public static void quickSort(int[] arr, int beg, int end) {
         comparison++;
-        if (end <= beg)
-        {
+        if (end <= beg) {
             return;
-        }
-        else
-        {
+        } else {
             //middle value of array
             int pivotIndex = partition(arr, beg, end);
             quickSort(arr, beg, pivotIndex -1);
             quickSort(arr, pivotIndex+1, end);
+            
             assignment += 3;
         }
     }
+    
     public static int partition(int[] arr, int beg, int end)
     {
         int pivotIndex = (int)(Math.random() * (end-beg)) + beg;
-        
         int swap = arr[end];
         arr[end] = arr[pivotIndex];
         arr[pivotIndex] = swap;
         
         int pivotVal = arr[end];
-        assignment += 4; 
-        
         int leftCur = beg;
         int rightCur = end - 1;
         
-        do
-        {
+        assignment += 4; 
+        
+        do {
             comparison++; 
-            while(arr[leftCur] < pivotVal)
-            {
+            while(arr[leftCur] < pivotVal) {
                 leftCur++;
                 comparison++;
             }
             comparison++;
-            while (arr[rightCur] > pivotVal && rightCur > beg)
-            {
+            while (arr[rightCur] > pivotVal && rightCur > beg) {
                 rightCur--;
+                
                 comparison++;
             }
             comparison++;
-            if (leftCur < rightCur)
-            {
+            if (leftCur < rightCur) {
                 int swap3 = arr[leftCur];
                 arr[leftCur] = arr[rightCur];
                 arr[rightCur] = swap3;
                 leftCur++;
+                
                 assignment += 3;
             }
             comparison++;
-        }
-        while(leftCur < rightCur);
-        
+        } while(leftCur < rightCur);
         //pivotIndex is a random number. End is where the pivot Value is Stored. 
         pivotIndex = leftCur;
+        int swap2 = arr[end];
+        arr[end] = arr[pivotIndex];
+        arr[pivotIndex] = swap2;
         
-         int swap2 = arr[end];
-         arr[end] = arr[pivotIndex];
-         arr[pivotIndex] = swap2; 
-         assignment += 3; 
-         return pivotIndex; 
+        assignment += 3; 
+    
+        return pivotIndex; 
     }
  }      
--- Test.java
+++ Test.java
@@ -1,64 +1,56 @@
-public class Test
-{
-    public static void testBubble()
-    {
-        int[] arr = {95, 80, 99, 88, 47, 77, 5, 37,  37, 9, 92, 87, 58, 65, 91, 28, 73, 59, 72, 48, 76, 85, 23, 36, 75, 93, 43, 8, 22, 96, 78, 33, 70, 16, 21, 2,
-             41, 6, 20, 7, 10, 98, 1, 42, 83, 71, 54, 67, 17, 50, 100, 49, 19};
+public class Test {
+    
+    public static void testBubble() {
+        int[] arr = {95, 80, 99, 88, 47, 77, 5, 37,  37, 9, 92, 87, 58, 65, 91, 28, 73, 59, 
+                     72, 48, 76, 85, 23, 36, 75, 93, 43, 8, 22, 96, 78, 33, 70, 16, 21, 2,
+                     41, 6, 20, 7, 10, 98, 1, 42, 83, 71, 54, 67, 17, 50, 100, 49, 19};
         Sorting.bubbleSort(arr);
-        String aStr = " ";
-        for (int i = 0; i < arr.length; i++)
-        {
-            aStr = aStr + arr[i] + " ";
+        String arrStr = " ";
+        for (int i = 0; i < arr.length; i++) {
+            arrStr = arrStr + arr[i] + " ";
         }
-        System.out.println(aStr);
+        System.out.println(arrStr);
         
     }
     
-    public static void testSelection()
-    {
+    public static void testSelection() {
         int[] arr = {95, 80, 99, 88, 47, 77, 5, 37,  37};
         Sorting.selectionSort(arr);
-        String aStr = " ";
-        for (int i = 0; i < arr.length; i++)
-        {
-            aStr = aStr + arr[i] + " ";
+        String arrStr = " ";
+        for (int i = 0; i < arr.length; i++) {
+            arrStr = arrStr + arr[i] + " ";
         }
-        System.out.println(aStr);
-        
+        System.out.println(arrStr);
     }
     
-    public static void testInsertion()
-    {
+    public static void testInsertion() {
         int[] arr = {95, 80, 99, 88, 47, 77, 5, 37,  37};
         Sorting.insertionSort(arr);
-        String aStr = " ";
-        for (int i = 0; i < arr.length; i++)
-        {
-            aStr = aStr + arr[i] + " ";
+        String arrStr = " ";
+        for (int i = 0; i < arr.length; i++) {
+            arrStr = arrStr + arr[i] + " ";
         }
-        System.out.println(aStr);
+        System.out.println(arrStr);
     }
-    public static void testMerge()
-    {
+    
+    public static void testMerge() {
         int[] arr = {95, 80, 99, 88, 47, 77, 5, 37,  37};
         Sorting.mergeSort(arr);
-        String aStr = " ";
-        for (int i = 0; i < arr.length; i++)
-        {
-            aStr = aStr + arr[i] + " ";
+        String arrStr = " ";
+        for (int i = 0; i < arr.length; i++) {
+            arrStr = arrStr + arr[i] + " ";
         }
-        System.out.println(aStr);
+        System.out.println(arrStr);
     }
-    public static void testQuickSort()
-    {
+    
+    public static void testQuickSort() {
         int[] arr = {95, 80, 99, 88, 47, 77, 5, 37,  8245};
         Sorting.quickSort(arr);
-        String aStr = " ";
-        for (int i = 0; i < arr.length; i++)
-        {
-            aStr = aStr + arr[i] + " ";
+        String arrStr = " ";
+        for (int i = 0; i < arr.length; i++) {
+            arrStr = arrStr + arr[i] + " ";
         }
-        System.out.println(aStr);
+        System.out.println(arrStr);
     }
 
 }
